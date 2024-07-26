14a8cd958802592e165f9a7dcbb42194349cb6f9
Wamitinewton <wamitinewton@gmail.com>
2024-07-26 13:36:53 +0300

    complete auth client

--- app/src/main/java/com/example/gauth/presentation/sign_in/GoogleAuthUiClient.kt
+++ app/src/main/java/com/example/gauth/presentation/sign_in/GoogleAuthUiClient.kt
@@ -1,11 +1,13 @@
 package com.example.gauth.presentation.sign_in
 
 import android.content.Context
+import android.content.Intent
 import android.content.IntentSender
 import com.example.gauth.R
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CancellationException
@@ -30,6 +32,50 @@ class GoogleAuthUiClient(
             null
         }
         return results?.pendingIntent?.intentSender
+    }
+
+    suspend fun signInWithIntent(intent: Intent): SignInResult {
+        val credential = oneTapClient.getSignInCredentialFromIntent(intent)
+        val googleIdToken = credential.googleIdToken
+        val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
+        return try {
+            val user = auth.signInWithCredential(googleCredentials).await().user
+            SignInResult(
+                data = user?.run {
+                    UserData(
+                        userId = uid,
+                        userName = displayName,
+                        profilePictureUrl = photoUrl?.toString()
+                    )
+                },
+                errorMessage = null
+            )
+        } catch (e: Exception){
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            SignInResult(
+                data = null,
+                errorMessage = e.message
+            )
+        }
+    }
+
+    suspend fun signOut(){
+        try {
+            oneTapClient.signOut().await()
+            auth.signOut()
+        } catch (e: Exception){
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+        }
+    }
+
+    fun getSignedInUser(): UserData? = auth.currentUser?.run {
+        UserData(
+            userId = uid,
+            userName = displayName,
+            profilePictureUrl = photoUrl?.toString()
+        )
     }
 
     private fun buildSigninRequest(): BeginSignInRequest {
