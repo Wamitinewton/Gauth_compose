100e3db1c9de89d2d13229e5b50691c82a0205d7
newton wamiti <144038101+Wamitinewton@users.noreply.github.com>
2024-07-27 23:47:13 +0300

    change

--- app/src/main/java/com/example/gauth/MainActivity.kt
+++ app/src/main/java/com/example/gauth/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GauthTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+       Gree
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -30,18 +30,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GauthTheme {
-        Greeting("Android")
-    }
-}
